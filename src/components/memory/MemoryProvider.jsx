78ed39984ed613744028e89142f665ff4296ce95
AurelienPospieszynski <aurelien.pos@hotmail.fr>
2022-12-02 16:30:55 +0100

    Gestion pairing cards + timeout

--- src/components/memory/MemoryProvider.jsx
+++ src/components/memory/MemoryProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { CARD_STATE, getInitialMemory } from "../../lib/memory";
+import { CARD_STATE, getInitialMemory, isPairCards } from "../../lib/memory";
 
 const MemoryContext = createContext();
 
@@ -39,7 +39,35 @@ export const MemoryContextProvider = ({ children }) => {
         return c;
       })
     );
+
+    if (returnedCards.length === 0) {
+      return;
+    }
+    returnedCards.push(returnedCard);
+    computeReturnedCards(returnedCards);
   };
+
+  const computeReturnedCards = (returnedCards) => {
+    const isPair = isPairCards(returnedCards[0], returnedCards[1]);
+
+    setTimeout(
+      () => {
+        setCards((current) => {
+          return current.map((card) => {
+            if (
+              card.state === CARD_STATE.RETURNED &&
+              returnedCards.includes(card)
+            ) {
+              card.state = isPair ? CARD_STATE.FIND : CARD_STATE.HIDE;
+            }
+            return card;
+          });
+        });
+      },
+      isPair ? 100 : 1500
+    );
+  };
+
   const reset = () => {
     console.log("je reset");
     setCards(getInitialMemory());
