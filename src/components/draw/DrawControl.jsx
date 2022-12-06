6fe71604718daada21e76319ad059ef6e15e560b
AurelienPospieszynski <aurelien.pos@hotmail.fr>
2022-12-06 11:24:52 +0100

    Canva done

--- src/components/draw/DrawControl.jsx
+++ src/components/draw/DrawControl.jsx
@@ -1,10 +1,16 @@
-// Draw exercise
-export const DrawControl = () => {
+export const DrawControl = ({
+  onColorChange,
+  onSizeChange,
+  defaultColor,
+  defaultSize,
+}) => {
   return (
     <div>
       <label
         htmlFor="draw-color-picker"
         className="flex items-center justify-center gap-4"
+        defaultValue={defaultColor}
+        onChange={(event) => onColorChange(event.target.value)}
       >
         Color
         <input id="draw-color-picker" type="color" />
@@ -12,6 +18,8 @@ export const DrawControl = () => {
       <label
         htmlFor="draw-size-picker"
         className="flex items-center justify-center gap-4"
+        defaultValue={defaultSize}
+        onChange={(event) => onSizeChange(event.target.value)}
       >
         Line size
         <input id="draw-size-picker" type="range" min="2" max="32" step="2" />
