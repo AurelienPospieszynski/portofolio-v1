12d19f39e5c85fc27674bdc8180e8f3354492989
AurelienPospieszynski <aurelien.pos@hotmail.fr>
2022-12-06 14:51:46 +0100

    fetch github pinned projects

--- src/components/project/ProjectSection.jsx
+++ src/components/project/ProjectSection.jsx
@@ -2,30 +2,40 @@ import { SectionWrapper } from "../atom/SectionWrapper";
 import { Project } from "./Project";
 import { GITHUB_USERNAME } from "../../lib/config";
 import { getListOfUrlRepositoriesUrl } from "../../lib/api-url";
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import { Loader } from "../atom/Loader/Loader";
 export const ProjectSection = () => {
+  const [projects, setProjects] = useState(null);
+  const [isLoading, setIsLoading] = useState();
+  const [error, setError] = useState(null);
+
   useEffect(() => {
+    setIsLoading(true);
     fetch(getListOfUrlRepositoriesUrl(GITHUB_USERNAME))
       .then((r) => r.json())
-      .then((repo) => console.log(repo));
+      .then((repo) => setProjects(repo))
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
-  const projects = [
-    {
-      name: "DEMO",
-      description: "DEMO",
-      stargazerCount: 12,
-      url: "https://github.com",
-      homepageUrl: "https://github.com",
-    },
-  ];
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p>Error !Might be the github token</p>;
+  }
 
   return (
     <SectionWrapper title="Projects">
       <div className="flex flex-wrap justify-center gap-8">
-        {/* GitHub Repository - Exercise (replace this) */}
-        <Project {...projects[0]} />
+        {projects?.map((repository) => {
+          return <Project key={repository.name} {...repository} />;
+        })}
       </div>
     </SectionWrapper>
   );
