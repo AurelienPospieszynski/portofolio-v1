1c98876ea70c8623c5a24d322d5409fead4dfdec
AurelienPospieszynski <aurelien.pos@hotmail.fr>
2022-12-06 13:24:22 +0100

    add tokens

--- src/components/project/ProjectSection.jsx
+++ src/components/project/ProjectSection.jsx
@@ -1,15 +1,23 @@
-import { SectionWrapper } from '../atom/SectionWrapper';
-import { Project } from './Project';
+import { SectionWrapper } from "../atom/SectionWrapper";
+import { Project } from "./Project";
+import { GITHUB_USERNAME } from "../../lib/config";
+import { getListOfUrlRepositoriesUrl } from "../../lib/api-url";
+import { useEffect } from "react";
 
 export const ProjectSection = () => {
-  // GitHub Repository - Exercise
+  useEffect(() => {
+    fetch(getListOfUrlRepositoriesUrl(GITHUB_USERNAME))
+      .then((r) => r.json())
+      .then((repo) => console.log(repo));
+  }, []);
+
   const projects = [
     {
-      name: 'DEMO',
-      description: 'DEMO',
+      name: "DEMO",
+      description: "DEMO",
       stargazerCount: 12,
-      url: 'https://github.com',
-      homepageUrl: 'https://github.com',
+      url: "https://github.com",
+      homepageUrl: "https://github.com",
     },
   ];
 
